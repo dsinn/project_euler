d31faecef0ac2d7d81f86530cb4b67ee12c27888
Daniel Sinn <daniel.sinn@horizn.com>
2018-09-11 08:12:18 -0400

    Optimize #12 slightly

--- src/012.rb
+++ src/012.rb
@@ -1,39 +1,14 @@
-def hasDivisors(x, amount)
-    # Inefficient but simple implementation
-    total = 1
-    # Even numbers first
-    while x & 1 == 0
-        x >>= 1
-        total += 1
-    end
+require_relative 'divisors.rb'
 
-    # Then odd
-    factor = 3
-    while x > 1
-        count = total
-        while x % factor == 0
-            total += count
-            x /= factor
-        end
-        factor += 2
+limit = 500
+sum = 1
+increment = 2
+loop do
+    sum += increment
+    increment += 1
+    #puts sum.to_s + ' -> ' + Divisors.count(sum).to_s
+    if Divisors.count(sum) >= limit
+        puts sum
+        break
     end
-
-    if x > 1
-        total += 1
-    end
-    total >= amount
 end
-
-# log2(500).ceil = 9, so find first triangular number at least 2^9 = 512 by solving for n(n+1)/2 = 512
-# n^2 + n - 1024 = 0
-a = 1
-b = 1
-c = -1024
-n = ((-b + Math.sqrt(b * b - 4 * a * c)) / (2.0 * a)).ceil
-
-sum = n * (n + 1) / 2
-while !hasDivisors(sum, 501)
-    n += 1
-    sum += n
-end
-puts sum
